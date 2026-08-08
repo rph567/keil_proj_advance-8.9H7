34292716b9c96dfaf533ab1565a47e4aa750daf6
rph567 <761885982@qq.com>
2026-08-08 20:24:01 +0800

    feat: extend led api

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -109,9 +109,9 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    led_on();
+    led_on(0U);
     HAL_Delay(250U);
-    led_off();
+    led_off(0U);
     HAL_Delay(250U);
   }
   /* USER CODE END 3 */
--- hardware/src/led.c
+++ hardware/src/led.c
@@ -4,14 +4,59 @@
  */
 #include "led.h"
 
-/* 点亮 LED1 */
-void led_on(void)
+inline uint8_t check(uint8_t led_num)
 {
-    HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_SET);
+    if (led_num < 1 || led_num > 4)
+    {
+        return 0; // 无效的 LED 编号
+    }
+    return 1; // 有效的 LED 编号
+}
+/* 点亮指定 LED */
+void led_on(uint8_t led_num)
+{
+    if (!check(led_num))
+    {
+        return; // 无效的 LED 编号
+    }
+
+    switch (led_num)
+    {
+        case 1:
+            HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_SET);
+            break;
+        case 2:
+            HAL_GPIO_WritePin(LED_GPIO_PORT, LED2_PIN, GPIO_PIN_SET);
+            break;
+        case 3:
+            HAL_GPIO_WritePin(LED_GPIO_PORT, LED3_PIN, GPIO_PIN_SET);
+            break;
+        case 4:
+            HAL_GPIO_WritePin(LED_GPIO_PORT, LED4_PIN, GPIO_PIN_SET);
+            break;
+        default:
+
+            break;
+    }
 }
 
-/* 熄灭 LED1 */
-void led_off(void)
-{
-    HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_RESET);
+/* 熄灭指定 LED */
+void led_off(uint8_t led_num)
+{    switch (led_num)
+    {
+        case 1:
+            HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_RESET);
+            break;
+        case 2:
+            HAL_GPIO_WritePin(LED_GPIO_PORT, LED2_PIN, GPIO_PIN_RESET);
+            break;
+        case 3:
+            HAL_GPIO_WritePin(LED_GPIO_PORT, LED3_PIN, GPIO_PIN_RESET);
+            break;
+        case 4:
+            HAL_GPIO_WritePin(LED_GPIO_PORT, LED4_PIN, GPIO_PIN_RESET);
+            break;
+        default:
+            break;
+    }
 }
